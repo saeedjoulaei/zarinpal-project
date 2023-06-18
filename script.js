0424f7375ca40d1256324d1e204e5092a14e7a8d
saeed joulaei <saeedjoulaei86@gmail.com>
2023-06-18 19:33:43 +0330

    hamburger menu added

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const hamburgerIcon = document.querySelector(".hamburger-icon");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const crossIcon = document.querySelector(".cross-icon");
+hamburgerIcon.addEventListener("click", function () {
+  console.log(hamburgerMenu.style.transform);
+  hamburgerMenu.classList.add("show-hamburger-menu");
+});
 
+crossIcon.addEventListener("click", function () {
+  hamburgerMenu.classList.remove("show-hamburger-menu");
+});
